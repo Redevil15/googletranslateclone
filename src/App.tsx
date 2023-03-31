61c2ff8b39d7ec3c1e45c799cbecf1f55b30eb56
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-03-30 21:37:31 -0600

    Inizializating project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useStore } from "./hooks/useStore";
+
+function App() {
+  const { fromLanguage, setFromLanguage } = useStore();
+
+  return (
+    <div className="App">
+      <h1>Google Translate</h1>
+      <button
+        onClick={() => {
+          setFromLanguage("es");
+        }}
+      >
+        Cambiar a espanol
+      </button>
+      {fromLanguage}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const SUPORTED_LANGUAGES = {
+    en: 'English',
+    es: 'Spanish',
+    fr: 'French',
+    de: 'German',
+    it: 'Italian',
+    ja: 'Japanese',
+    ko: 'Korean',
+}
+
+export const AUTO_LANGUAGE = 'auto'
